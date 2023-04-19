896019ad816f3c3c07b673f87a7b0c0e251f04e3
Epiphany6666 <115459047+Epiphany6666@users.noreply.github.com>
2023-04-19 19:47:45 +0800

    第1章

---
+++ charpter01/Hello.java
@@ -0,0 +1,13 @@
+//这是java的快速入门，演示java的开发步骤
+
+//1.public class Hello 表示Hello是一个类，是一个public公有的类
+//2.Hello{}表示一个类的开始和结束
+//3.public static void main（string[] args）表示一个主方法，即我们程序的入口
+//4.main（）{}表示方法的开始和结束
+public class Hello {
+	
+	//编写一个main方法
+	public static void main(String[] args){
+	System.out.println("hello,world~");
+	}
+}
---
+++ charpter01/HomeWork01.java
@@ -0,0 +1,5 @@
+public class HomeWork01{
+	public static void main(String[] args){
+		System.out.println("xiaofeng\ngirl\nÖĞ¹ú\nºşÄÏ");
+	}
+}
