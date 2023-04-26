93bc55aa22aad5a8d8750fbfb9f68cd5e6c60364
Epiphany6666 <115459047+Epiphany6666@users.noreply.github.com>
2023-04-26 19:52:12 +0800

    Add files via upload

---
+++ charpter04/ForExercise01.java
@@ -0,0 +1,20 @@
+public class ForExercise01{
+	public static void main(String[] args){
+		int i = 0;
+		int n = 10;
+		/*int j = 0;
+		for(i = 0; i <= 5; i++){
+			for(j = 5; j >= 0; j--){
+				System.out.println(i + '+' + j + '=' + (i+j));
+			}
+		}
+	}for(i = 0; i <= n; i++){
+		System.out.println(i + "+" + (5 - i) + "=5");
+		}
+	}*/
+	//将5变成一个变量
+	for(i = 0; i <= n; i++){
+		System.out.println(i + "+" + (n - i) + "=" + n);
+		}
+	}
+}
