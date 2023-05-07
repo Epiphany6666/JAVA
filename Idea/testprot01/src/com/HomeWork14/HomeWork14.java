bf8a641b0e034bd7d04409da96435d7b1e8ffed3
Epiphany6666 <115459047+Epiphany6666@users.noreply.github.com>
2023-05-07 11:18:29 +0800

    Add files via upload

---
+++ Idea/testprot01/src/com/HomeWork14/HomeWork14.java
@@ -0,0 +1,13 @@
+package com.HomeWork.HomeWork14;
+
+public class HomeWork14 {
+    /**
+     * 我是A类
+     * 我是B类的无参构造
+     * 我是B类的无参构造
+     * 我是A类
+     * hahah我是B类的有参构造
+     * 我是C类的有参构造
+     * 我是C类的无参构造
+     */
+}
