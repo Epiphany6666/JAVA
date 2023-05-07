e34a2a1efcc36655522afb068ca9b9b416cf3a88
Epiphany6666 <115459047+Epiphany6666@users.noreply.github.com>
2023-05-07 11:18:00 +0800

    Add files via upload

---
+++ Idea/testprot01/src/com/HomeWork12/HomeWork12.java
@@ -0,0 +1,8 @@
+package com.HomeWork.HomeWork12;
+
+public class HomeWork12 {
+    /**
+     * ==是比较运算符，如果判断基本数据，就是判断值是否相等；如果是判断引用类型，就是判断地址（对象）是否相等
+     * equals是Object方法，只能判断引用类型对象是否相等，子类常常重写该方法，判断内容是否相等
+     */
+}
