626aab4ceb0fdc2593ea750bcf368411b4081240
Epiphany6666 <115459047+Epiphany6666@users.noreply.github.com>
2023-05-07 11:18:38 +0800

    Add files via upload

---
+++ Idea/testprot01/src/com/HomeWork15/HomeWork15.java
@@ -0,0 +1,11 @@
+package com.HomeWork.HomeWork15;
+
+public class HomeWork15 {
+    /**
+     * 方法重载
+     * 方法重写
+     * 对象多态
+     * 向上转型
+     * 向下转型
+     */
+}
---
+++ Idea/testprot01/src/com/HomeWork16/HomeWork16.java
@@ -0,0 +1,8 @@
+package com.HomeWork.HomeWork16;
+
+public class HomeWork16 {
+    /**
+     * 当调用对象方法的时候，该方法会和对象的地址（在堆里的地址）即运行类型相绑定
+     * 调用属性的时候没有内存绑定机制，哪里声明哪里使用
+     */
+}
